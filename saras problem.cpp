369c23d4f164e9b6f31d16bbfedbc9b4a69550f6
Parth Shukla <97020896+parth9643@users.noreply.github.com>
2023-10-28 20:33:54 +0530

    Update saras problem.cpp

--- saras problem.cpp
+++ saras problem.cpp
@@ -4,7 +4,6 @@ public:
         int totalBoxes = 0;
         ListNode* current = head;
         
-        // Count the total number of boxes
         while (current != nullptr) {
             totalBoxes++;
             current = current->next;
@@ -14,12 +13,10 @@ public:
         int chocolatesReceived = 0;
         int maxChocolatesReceived = 0;
         
-        // Traverse the list and distribute chocolates
         for (int i = 0; i < totalBoxes / 2; i++) {
             chocolatesReceived += current->val + current->next->val;
             current = current->next->next;
             
-            // Update the maximum if needed
             maxChocolatesReceived = std::max(maxChocolatesReceived, chocolatesReceived);
         }
         
